30dd56b522cf47c3a6d8ee8ffe5f82b34469a0cb
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2020-11-06 15:27:19 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 for conf in tmux.conf inputrc bash_aliases vimrc; do
 	cp -v $(dirname $0)/$conf ~/.$conf
 done
+if [ -f ~/.bashrc ]; then
+	sed -i 's/^#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
+fi
