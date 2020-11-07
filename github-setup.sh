aeaed14b86c846ff10f801ca0c7e934671795c0b
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2020-11-07 02:48:06 +0000

    Added github setup
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

---
+++ github-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# username and email
+git config --global user.name "Bob Schmitz III"
+git config --global user.email 14095796+rgschmitz1@users.noreply.github.com
+# prefered editor is vim
+git config --global core.editor vim
+# git command alias
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.di diff
+git config --global alias.up 'fetch upstream'
+
