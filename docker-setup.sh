41ba9f67593889ba72911a71c3927c62848e9aee
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-08-31 17:15:38 +0000

    Update docker-setup to support docker-compose
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- docker-setup.sh
+++ docker-setup.sh
@@ -10,13 +10,16 @@ sudo apt-get update && sudo apt-get install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	$(lsb_release -cs) \
-	stable"
-sudo apt update && sudo apt install -y \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update && sudo apt-get install -y \
 	docker-ce docker-ce-cli containerd.io
 sudo docker run --rm hello-world
 sudo docker rmi hello-world:latest
 
 # Setup so that Docker can be run without sudo
 sudo usermod -aG docker `whoami`
+
+if [ ! which docker-compose > /dev/null ]; then
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+	sudo chmod +x /usr/local/bin/docker-compose
+fi
