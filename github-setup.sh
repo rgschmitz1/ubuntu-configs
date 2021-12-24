364f235063f7bdc00d907350d1a9790beddf680f
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-12-24 19:46:26 +0000

    Updating github-setup to do more fine grained checks
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- github-setup.sh
+++ github-setup.sh
@@ -15,6 +15,34 @@ create_passphrase() {
 	fi
 }
 
+git_pass_helper() {
+	# Verify pass is installed
+	if ! which pass > /dev/null; then
+		sudo apt update && \
+		sudo apt install -y pass
+	fi
+	pass init $key
+	# Check if dev/github is valid
+	if ! pass dev/github > /dev/null; then
+		echo enter password on first line and username on second line
+		pass insert -m dev/github
+	fi
+	# setup credential store to manage access tokens
+	local pass_helper=/usr/local/bin/git-pass-helper
+	git config --global credential.helper $pass_helper
+	local temp=$(mktemp)
+	cat <<-'PASS-HELPER' > $temp
+	#!/bin/bash
+	if [[ -n "$1" && $1 = 'get' ]]; then
+		credentials=($(pass dev/github))
+		echo "username=${credentials[1]}"
+		echo "password=${credentials[0]}"
+	fi
+	PASS-HELPER
+	sudo mv $temp $pass_helper
+	sudo chmod 755 $pass_helper
+}
+
 git config --global user.name $USERNAME
 git config --global user.email $EMAIL
 # preferred editor is vim
@@ -27,34 +55,25 @@ git config --global alias.st status
 git config --global alias.di diff
 git config --global alias.pu pull
 git config --global alias.up 'fetch upstream'
-# setup credential store to manage access tokens
-pass_helper=/usr/local/bin/git-pass-helper
-git config --global credential.helper $pass_helper
-cat << 'PASS-HELPER' > /tmp/git-pass-helper
-#!/bin/bash
-if [[ -n "$1" && $1 = 'get' ]]; then
-	credentials=($(pass dev/github))
-	echo "username=${credentials[1]}"
-	echo "password=${credentials[0]}"
+
+# GPG_TTY needs to be set or the passphrase prompt will not appear in terminal
+if ! grep -q GPG_TTY ~/.bashrc; then
+	echo 'export GPG_TTY=$(tty)' >> ~/.bashrc
+	export GPG_TTY=$(tty)
 fi
-PASS-HELPER
-sudo mv /tmp/git-pass-helper $pass_helper
-sudo chmod 755 $pass_helper
-unset pass_helper
-grep -q GPG_TTY ~/.bashrc || echo 'export GPG_TTY=$(tty)' >> ~/.bashrc
-if ! which pass > /dev/null; then
-	sudo apt update && \
-	sudo apt install -y pass && \
+# Verify we a gpg key has been initialized
+key='gpg --list-key "$USERNAME" 2> /dev/null | awk "/^pub/{getline;print}" | xargs'
+if ! gpg --list-key "$USERNAME" &> /dev/null; then
 	create_passphrase
 	gpg --batch --passphrase "$passphrase" --quick-gen-key "$USERNAME <$EMAIL>" rsa4096 default 0 && \
-	key=$(gpg --list-key "$USERNAME" 2> /dev/null | awk '/^pub/{getline;print}' | xargs)
+	key=$(eval $key)
 	if [ -z "$key" ]; then
 		echo "ERROR: gpg key is empty, check setup and try again."
 		exit 1
 	fi
 	gpg --batch --passphrase "$passphrase" --quick-add-key $key rsa4096 default 0
-	pass init $key
-	unset key
+else
+	key=$(eval $key)
 fi
-echo enter password on first line and username on second line
-pass insert -m dev/github
+# Create pass helper for git
+git_pass_helper
