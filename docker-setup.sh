47daa6f943a30de22cd1a5f7eb7ebe4e99423516
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2022-03-08 06:43:53 +0000

    Get the latest version of docker-compose
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- docker-setup.sh
+++ docker-setup.sh
@@ -33,6 +33,6 @@ sudo usermod -aG docker `whoami`
 
 # Install docker-compose
 if ! which docker-compose > /dev/null; then
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+	sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
 	sudo chmod +x /usr/local/bin/docker-compose
 fi
